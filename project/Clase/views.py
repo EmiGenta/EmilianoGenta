927080168d7dd44f699a61d9480d4130a18ee183
Emiliano Genta <emigenta@gmail.com>
2024-04-27 16:18:45 -0300

    add: urls

--- project/Clase/views.py
+++ project/Clase/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+
 from . import models
 
 # Creamos el home
 def home(request):
-    ...
+    query = models.Comision.objects.all()
+    context = {"comisiones": query} #consulta a la bdd
+    return render(request, "Clase/index.html", context)
--- project/core/urls.py
+++ project/core/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from . import views
 
-app_name = 'Clase'
+app_name = 'core'
 
 urlpatterns = [
     path('', views.home, name='home'),
